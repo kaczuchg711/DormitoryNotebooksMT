dc5d0305f51dbb263446a916f8371edacd194baa
Tomasz Kacza <kaczatom4@gmail.com>
2024-01-08 18:02:10 +0100

    start making login page

--- Java/DormitoryNotebooksJava/src/main/java/pl/kaczuchg711/DormitoryNotebooksJava/organizations/OrganizationRepository.java
+++ Java/DormitoryNotebooksJava/src/main/java/pl/kaczuchg711/DormitoryNotebooksJava/organizations/OrganizationRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
 
     Optional<Organization> findByAcronym(String organizationAcronym);
+    Optional<Organization> findById(Integer id);
 }
--- Java/DormitoryNotebooksJava/src/main/java/pl/kaczuchg711/DormitoryNotebooksJava/security/SecurityController.java
+++ Java/DormitoryNotebooksJava/src/main/java/pl/kaczuchg711/DormitoryNotebooksJava/security/SecurityController.java
@@ -1,21 +1,44 @@
 package pl.kaczuchg711.DormitoryNotebooksJava.security;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import pl.kaczuchg711.DormitoryNotebooksJava.organizations.Organization;
+import pl.kaczuchg711.DormitoryNotebooksJava.organizations.OrganizationRepository;
+
+import java.util.Optional;
 
 @Controller
 public class SecurityController {
-
+    @Autowired
+    private OrganizationRepository organizationRepository;
     @GetMapping("/")
-    @ResponseBody // Add this if you want to return a text response directly
-    public String handleOrganizationsUrlRequest(HttpSession session) {
+    public ModelAndView handleStartPageUrlRequest(HttpSession session, Model model) {
+        return processOrganizationCheck(session, model);
+    }
+
+    @GetMapping("/login")
+    public ModelAndView fun(HttpSession session, Model model) {
+        return processOrganizationCheck(session, model);
+    }
+
+    private ModelAndView processOrganizationCheck(HttpSession session, Model model) {
         Integer organizationId = (Integer) session.getAttribute("organization_id");
+
         if (organizationId == null) {
-            // Redirect to main page or show a message if organization is not set
-            return "No organization selected. Redirecting to main page...";
+            return new ModelAndView("redirect:/organizations");
         }
-        return "Logged in with organization ID: " + organizationId;
+        Optional<Organization> organizationOptional = organizationRepository.findById(organizationId);
+        if (organizationOptional.isPresent()) {
+            Organization organization = organizationOptional.get();
+            model.addAttribute("organization_id", organization.getId());
+            model.addAttribute("organization_acronym", organization.getAcronym());
+            return new ModelAndView("login");
+        } else {
+            return new ModelAndView("redirect:/organizations");
+        }
     }
 }
